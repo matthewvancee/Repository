0564c2a538e588750a168d1cdaad6189696db83f
matthewvancee <matthewvancee@gmail.com>
2025-04-20 20:09:44 -0400

    0-2

--- Project 0-1.py
+++ Project 0-1.py
@@ -6,19 +6,29 @@ def display_menu():
     print("********************************")
     print("Please Enter the following number below from the following menu:")
     print("1. PRINT all Authorized Vehicles")
-    print("2. Exit")
+    print("2. SEARCH for Authorized Vehicle")
+    print("3. Exit")
 #If 1, print all authorized vehicles
 def print_vehicles():
     print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
     for vehicle in AllowedVehiclesList:
         print(vehicle)
-#Loop to display menu and get user input, if 2 then exit
+#If 2, search for a vehicle
+def search_vehicle(vehicle_name):
+    if vehicle_name in AllowedVehiclesList:
+        print(f"{vehicle_name} is an authorized vehicle.")
+    else:
+        print(f"{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again.")
+#Loop to display menu and get user input, if 3 then exit
 while True: 
     display_menu()
     choice = input("Please enter your choice: ")
     if choice == '1':
         print_authorized_vehicles = print_vehicles()
     elif choice == '2':
+        vehicle_name = input("Please Enter the full vehicle name: ")
+        search_vehicle(vehicle_name)
+    elif choice == '3':
         print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
         break
     else:
