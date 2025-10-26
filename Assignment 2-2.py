e0390a9b29f9c56bd25584d149494cd8e0351c71
Matthew Vance <matthewvancee@gmail.com>
2025-10-26 18:02:40 -0400

    Clean

--- Assignment 2-2.py
+++
@@ -1,5 +0,0 @@
-myCurrentAge = 20
-currentYear = 2025
-myNewAge = myCurrentAge + (2050 - currentYear)
-print("My Current Age is " + str(myCurrentAge))
-print("I will be " + str(myNewAge) + " in 2050.")
