ff628f53e992276d4a515519db0a97160152afa9
Matthew Vance <matthewvancee@gmail.com>
2025-10-26 18:05:46 -0400

    Clean

--- Class Ranker.py
+++ Class Ranker.py
@@ -1,9 +1,8 @@
 #Function: This program determines if a student will be admitted or rejected.
-#Input:  Interactive
-testScoreString = int(input("Enter the test score: ")) #Input: testScore
-classRankString = int(input("Enter the class rank: ")) #Input: classRank
-testScore = int(testScoreString) #Convert testScore to int
-classRank = int(classRankString) #Convert classRank to int
+testScoreString = int(input("Enter the test score: ")) 
+classRankString = int(input("Enter the class rank: ")) 
+testScore = int(testScoreString) 
+classRank = int(classRankString) 
 #Output: Accept or Reject
 
 # Get input and convert to correct data type for testScore and classRank
